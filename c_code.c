6c58e9eeb581b5049c8f065fd22b3c6efcd95b9c
Aishwarya-Manjunath <aishwarya.9711@gmail.com>
2018-05-12 14:15:36 +0530

    Includedprintf

--- c_code.c
+++ c_code.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<stdlib.h>
 int q = 10;
 int fun1(int a,int x)
 {
@@ -17,12 +19,16 @@ int main(void)
 	int i = 20 , j = 10, k = 2;
 	i = q+1;
 	q = 11;
+	int *heap = (int *)malloc(sizeof(int)*2);
+	//scanf("%d",&i);
+	printf("i is :%d",i);
 	int a[j][k];
 	int sum = i + j;
 	int z = i *6;
 	int *p = &z;
 	*p = 8;
-	*p = fun(i,89);
+	k = fun(i,89);
 	i = 10;
+	printf("value of p is :%d",*p);
 	return 0;
 }
--- interface.py
+++ interface.py
@@ -5,10 +5,15 @@ Enter file :<name of your c program to be visualised>
 '''
 
 '''
-return value?
-finish
-
 global variables
+'''
+
+'''
+scanf
+'''
+
+'''
+heap
 '''
 
 from subprocess import *
@@ -18,6 +23,7 @@ from fcntl import fcntl, F_GETFL, F_SETFL
 from os import O_NONBLOCK, read
 import string
 import re
+import sys
 
 stop = 0
 ret = 0
@@ -25,16 +31,17 @@ func = re.compile("\w+ \(((\w+\=\w+), )*(\w+\=\w+)?\)")
 
 my_file = raw_input('enter program name (with a ./ if in local directory): ')
 
-subprocess.call(["gcc","-g",my_file])
+subprocess.call(["gcc","-g","-static",my_file])
 
 p1 = Popen(['gdb', 'a.out'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 flags = fcntl(p1.stdout, F_GETFL) # get current p.stdout flags
 fcntl(p1.stdout, F_SETFL, flags | O_NONBLOCK)
 
+print 'Hit Enter to Begin'
 def output(p1,flag):
 	my_out = ''
-	sleep(0.3)
+	sleep(0.1)
 	while True:
 		try:
 			my_out = read(p1.stdout.fileno(), 1024)
@@ -45,6 +52,24 @@ def output(p1,flag):
 	#print "-------------------------------------------------"
 	#print my_out
 	#print "-------------------------------------------------"
+	if "printf" in my_out:
+		print "Output:"
+		op_string = my_out[11:len(my_out)-9].split(",")
+		i = 0
+		i_args = 1
+		format_string = op_string[0]
+		format_string = string.replace(format_string,'"','')
+		while i<len(format_string):
+			if format_string[i] == '%':
+				temp = 'print '+op_string[i_args]+'\n'
+				p1.stdin.write(temp)
+				output(p1,5)
+				i_args += 1
+				i +=2
+				continue
+			sys.stdout.write(format_string[i])
+			i += 1
+		print '\n'		
 	global ret
 	m = func.match(my_out)
 	if m is not None:
@@ -69,7 +94,13 @@ def output(p1,flag):
 	if 'return' in my_out:
 		ret = 1
 		return 
-	if flag == 2:
+	if flag == 5:
+		#print my_out
+		my_out = my_out.split('=')[1]
+		my_out = string.replace(my_out,'(gdb)','')
+		my_out = string.replace(my_out,'\n','')
+		sys.stdout.write(my_out)
+	elif flag == 2:
 		my_out = string.replace(my_out,'(gdb)','')
 		my_out = my_out.split(' ')
 		my_out = my_out[:2]
@@ -92,6 +123,10 @@ p1.stdin.write('run\n')
 output(p1,0)
 
 while True:
+	inp = raw_input()
+	#print inp
+	if(inp=='exit' or inp=='quit'):
+		break
 	p1.stdin.write('step\n')
 	output(p1,0)
 	p1.stdin.write('info line\n')
